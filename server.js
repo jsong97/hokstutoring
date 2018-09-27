d9ed784350703d4d03a5f26ec7c06ddcb3bfa79c
Justin Song <jsong97@outlook.com>
2018-09-27 16:07:17 -0400

    new changes to heroku

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const path = require('path');
 
 const app = express();
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.get('/api/customers', (req, res) => {
+  const count = 3;
+
   const customers = [
     {id: 1, firstName: 'John', lastName: 'Doe'},
     {id: 2, firstName: 'Brad', lastName: 'Traversy'},
@@ -11,8 +16,59 @@ app.get('/api/customers', (req, res) => {
   ];
 
   res.json(customers);
+  console.log(`Sent ${count} customers`);
 });
 
-const port = 5000;
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => `Server running on port ${port}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////
